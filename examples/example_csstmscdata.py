07b9bd93cfbf432bc81b17a010a9af649be87559
Bo Zhang <bozhang@nao.cas.cn>
2022-03-02 16:54:55 +0800

    updated example_csstmscdata.py

--- examples/example_csstmscdata.py
+++ examples/example_csstmscdata.py
@@ -1,7 +1,3 @@
-fp = "/Users/cham/projects/csst/examples/MSC_MS_210527171000_100000279_16_raw.fits"
-fp = "/Users/cham/projects/csst/examples/bias/MSC_CLB_210525120000_100000000_06_raw.fits"
-fp = "/Users/cham/projects/csst/examples/lam/MSC_CLB_210525120000_100000000_06_raw.fits"
-
 import os
 os.chdir("/Users/cham/projects/csst/test/")
 fp = "MSC_MS_210527171000_100000279_16_raw.fits"
