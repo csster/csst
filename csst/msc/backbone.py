d5cc7f24a17d97511cf05db771801546cbbad23b
Bo Zhang <bozhang@nao.cas.cn>
2022-06-16 13:24:02 +0800

    updated backbone module

--- csst/msc/backbone.py
+++ csst/msc/backbone.py
@@ -1,7 +1,29 @@
-
-
 # define CCD ID list
 CCD_ID_LIST = [6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25]
 
-# version of simulation
+FILTER_LIST = ['N', 'u', 'g', 'r', 'i', 'z', 'y']
+
+# define CCD->FILTER mapping
+CCD_FILTER_MAPPING = {
+    6: "y",
+    7: "i",
+    8: "g",
+    9: "r",
+    11: "z",
+    12: "nuv",
+    13: "nuv",
+    14: "u",
+    15: "y",
+    16: "y",
+    17: "u",
+    18: "nuv",
+    19: "nuv",
+    20: "z",
+    22: "r",
+    23: "g",
+    24: "i",
+    25: "y",
+}
+
+# define versions of simulation
 VER_SIMS = ["C5.1", "C3"]
